47f0eda6320dbbf44c9cded425b479f72cb15360
HildaAb <ilona.aase@gmail.com>
2025-03-02 11:04:22 +0100

    added js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const form = document.querySelector("#form");
-const username = document.querySelector("#username");
-const email = document.querySelector("#email");
-const password = document.querySelector("#password");
-const password2 = document.querySelector("#password2");
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -16,23 +16,49 @@ function checkInput() {
   const passwordValue = password.value.trim();
   const password2Value = password2.value.trim();
 
-  if (username === "") {
-    setErrorFo(username, "Username can not be blanc");
+  if (usernameValue === "") {
+    setErrorFor(username, "Username can not be blank");
   } else {
     setSuccessFor(username);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "Email can not be blank");
+  } else if (!IsEmail(emailValue)) {
+    setErrorFor(email, "Email is not valid");
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (passwordValue === "") {
+    setErrorFor(password, "Password can not be blank");
+  } else {
+    setSuccessFor(password);
+  }
+
+  if (password2Value === "") {
+    setErrorFor(password2, "Password can not be blanc");
+  } else if (passwordValue !== password2Value) {
+    setErrorFor(password2, "Passwords does not match");
+  } else {
+    setSuccessFor(password2);
   }
 }
 
 function setErrorFor(input, message) {
-  const formControl = input.parentELement;
+  const formControl = input.parentElement;
   const small = formControl.querySelector("small");
 
-  small.innerText = message;
-
+  small.innerHTML = message;
   formControl.className = "form-control error";
 }
 
 function setSuccessFor(input) {
   const formControl = input.parentElement;
+
   formControl.className = "form-control success";
+}
+
+function IsEmail(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 }
